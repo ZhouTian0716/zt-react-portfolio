5d66306e89bcf6e33830e90704a3ffb56fb9b325
ZhouTian0716 <egglord0716@gmail.com>
2021-07-18 10:46:36 +1000

    link issue

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,9 +4,9 @@ export default function footer() {
     return (
         <div className="footer">
             
-            <a href="https://github.com/ZhouTian0716" target="_blank"><i class="fab fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/zhou-anthony-tian-19b017143/" target="_blank"><i class="fab fa-linkedin"></i></a>
-            <a href="https://stackoverflow.com/users/15256660/zhou-tian" target="_blank"><i class="fab fa-stack-overflow"></i></a>
+            <a href="https://github.com/ZhouTian0716" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/zhou-anthony-tian-19b017143/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
+            <a href="https://stackoverflow.com/users/15256660/zhou-tian" target="_blank" rel="noreferrer"><i class="fab fa-stack-overflow"></i></a>
             
             
         </div>
