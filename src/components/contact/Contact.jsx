5b8248172a6e71217756394a2d81e8df2814b6b8
ZhouTian0716 <egglord0716@gmail.com>
2021-07-20 15:58:46 +1000

    style and resposive pending

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,16 +1,77 @@
+import { useState } from 'react'
 import "./Contact.scss"
+import { validateEmail } from '../../utils/helpers';
 
-export default function contact() {
+export default function Contact() {
+
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [textArea, setTextArea] = useState('');
+    const [message, setMessage] = useState('Please complete form');
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'userName') {
+            setUserName(inputValue);
+        } else if (inputType === 'textArea') {
+            setTextArea(inputValue);
+        };
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!userName) {
+            setMessage('User name required!');
+            return;
+        };
+        if (!validateEmail(email)) {
+            setMessage('Email is not valid!');
+            return;
+        };
+        if (!textArea) {
+            setMessage('Message required!');
+            return;
+        };
+          
+        setMessage('Message Sended!');
+        return;
+      };
+    
+
     return (
         <form className="contact">
             <h1>Contact</h1>
+            <h3>{message}</h3>
             <h3>Name:</h3>
-            <input type="text" required/>
+            <input
+                value={userName}
+                name="userName"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="username"
+            />
             <h3>Email address:</h3>
-            <input type="email" required/>
+            <input
+                value={email}
+                name="email"
+                onChange={handleInputChange}
+                type="email"
+                placeholder="email"
+            />
             <h3>Message:</h3>
-            <textarea type="text" required/>
-            <button>Submit</button>
+            <textarea
+                value={textArea}
+                name="textArea"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="Your Message"
+            />
+            <button type="button" onClick={handleFormSubmit}>Submit</button>
         </form>
     )
 }
--- src/components/portfolio/card/Card.jsx
+++ src/components/portfolio/card/Card.jsx
@@ -1,11 +1,7 @@
-
 import "./Card.scss"
 
 export default function card({ id, title, deployUrl, repoUrl }) {
-  
-
     return (
-
         <div className="card">
             <button><a href={deployUrl} target="_blank" rel="noreferrer">{title}</a></button>
             <img src={require(`./img/${id}.jpg`).default} height={300} width={400} alt={title}/>
